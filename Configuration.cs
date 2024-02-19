4814767fb3ecee063a69ae00b60acaba7398f067
branea <48945844+branea@users.noreply.github.com>
2024-02-19 15:00:58 +0200

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Test_Framework
+{
+    internal static class Configuration
+    {
+        public static IConfiguration AppSetting { get; }
+        public static string ApplicationUrl => AppSetting["applicationUrl"];
+        public static string UserName => AppSetting["username"];
+        public static string Password => AppSetting["password"];
+
+        static Configuration()
+        {
+            AppSetting = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("specflow.json")
+                .Build();
+        }
+        
+
+    }
+}
